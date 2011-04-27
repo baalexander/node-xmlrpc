2fcb216d96edb0eeb38d743e8b3eac86f4585d01
Brandon Alexander <baalexander@gmail.com>
2011-04-27 18:37:54 -0500

    Adds support for XML-RPC's struct and nil to message calls.

--- example/client.js
+++ example/client.js
@@ -7,6 +7,6 @@ http.createServer(function (req, res) {
 
 var client = xmlrpc.createClient()
 console.log(client)
-client.call('dosomething', ['param1', 2, 2.2, true, false, ['a', 'b', 'c']], function () { })
+client.call('dosomething', ['param1', 2, 2.2, true, false, ['a', 'b', 'c'], { a: 'objparam1', b2: 'objectparam2' }, null], function () { })
 console.log('Started the XMLRPC test client')
 

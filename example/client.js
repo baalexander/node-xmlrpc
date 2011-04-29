9b85e8e677eb6895753ceb88fd7d56ed4dbedccf
Brandon Alexander <baalexander@gmail.com>
2011-04-28 21:36:04 -0500

    Client now post to the http server and get response.
    Tested with ROS's [Master API](http://www.ros.org/wiki/ROS/Master_API). Code is rough. Needs cleaned up.

--- example/client.js
+++ example/client.js
@@ -5,8 +5,9 @@ http.createServer(function (req, res) {
 
 }).listen(3000, 'localhost')
 
-var client = xmlrpc.createClient()
+var client = xmlrpc.createClient(http)
 console.log(client)
-client.call('dosomething', ['param1', 2, 2.2, true, false, ['a', 'b', 'c'], { a: 'objparam1', b2: 'objectparam2' }, null, new Date()], function () { })
-console.log('Started the XMLRPC test client')
+//client.call('dosomething', ['param1', 2, 2.2, true, false, ['a', 'b', 'c'], { a: 'objparam1', b2: 'objectparam2' }, null, new Date()], function () { })
+client.call('getSystemState', '/', function () {})
+
 

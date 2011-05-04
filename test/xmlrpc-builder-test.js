449cf10884a4799c23468f143809df098f9a3922
Brandon Alexander <baalexander@gmail.com>
2011-05-03 20:08:49 -0500

    Organizes the builder test by data type alphabetically.
    I know, OCDing out here, but the other tests are already organized this way.

--- test/xmlrpc-builder-test.js
+++ test/xmlrpc-builder-test.js
@@ -7,65 +7,13 @@ vows.describe('XML-RPC Builder').addBatch({
   // Test buildMethodCall functionality
   //////////////////////////////////////////////////////////////////////
   'A buildMethodCall call' : {
-    // Test String
-    'with a regular String param' : {
-      topic: function() {
-        xmlrpcBuilder.buildMethodCall('testMethod', ['testString'], this.callback)
-      }
-    , 'contains the string' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><string>testString</string></value></param></params></methodCall>')
-      }
-    }
-    // FIXME Empty string causes warnings. Need to figure out what's the
-    // spec way to define an empty string
-  , 'with an empty String param' : {
+    // Test Array
+    'with an Array param' : {
       topic: function () {
-        xmlrpcBuilder.buildMethodCall('testMethod', [''], this.callback)
+        xmlrpcBuilder.buildMethodCall('testMethod', [['string1', 3]], this.callback)
       }
-    , 'contains an empty string' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><string/></value></param></params></methodCall>')
-      }
-    }
-    // Test Integer
-  , 'with a positive Interger param' : {
-      topic: function () {
-        xmlrpcBuilder.buildMethodCall('testMethod', [17], this.callback)
-      }
-    , 'contains the integer' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><int>17</int></value></param></params></methodCall>')
-      }
-    }
-  , 'with a negative Integer param' : {
-      topic: function () {
-        xmlrpcBuilder.buildMethodCall('testMethod', [-32], this.callback)
-      }
-    , 'contains the negative integer' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><int>-32</int></value></param></params></methodCall>')
-      }
-    }
-  , 'with an Integer param of 0' : {
-      topic: function () {
-        xmlrpcBuilder.buildMethodCall('testMethod', [0], this.callback)
-      }
-    , 'contains 0' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><int>0</int></value></param></params></methodCall>')
-      }
-    }
-    // Test Double
-  , 'with a positive Double param' : {
-      topic: function () {
-        xmlrpcBuilder.buildMethodCall('testMethod', [17.5], this.callback)
-      }
-    , 'contains the double' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><double>17.5</double></value></param></params></methodCall>')
-      }
-    }
-  , 'with a negative Double param' : {
-      topic: function () {
-        xmlrpcBuilder.buildMethodCall('testMethod', [-32.7777], this.callback)
-      }
-    , 'contains the negative double' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><double>-32.7777</double></value></param></params></methodCall>')
+    , 'contains the array' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><array><data><value><string>string1</string></value><value><int>3</int></value></data></array></value></param></params></methodCall>')
       }
     }
     // Test Boolean
@@ -94,6 +42,48 @@ vows.describe('XML-RPC Builder').addBatch({
         assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><dateTime.iso8601>20120607T11:35:10</dateTime.iso8601></value></param></params></methodCall>')
       }
     }
+    // Test Double
+  , 'with a positive Double param' : {
+      topic: function () {
+        xmlrpcBuilder.buildMethodCall('testMethod', [17.5], this.callback)
+      }
+    , 'contains the double' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><double>17.5</double></value></param></params></methodCall>')
+      }
+    }
+  , 'with a negative Double param' : {
+      topic: function () {
+        xmlrpcBuilder.buildMethodCall('testMethod', [-32.7777], this.callback)
+      }
+    , 'contains the negative double' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><double>-32.7777</double></value></param></params></methodCall>')
+      }
+    }
+    // Test Integer
+  , 'with a positive Interger param' : {
+      topic: function () {
+        xmlrpcBuilder.buildMethodCall('testMethod', [17], this.callback)
+      }
+    , 'contains the integer' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><int>17</int></value></param></params></methodCall>')
+      }
+    }
+  , 'with a negative Integer param' : {
+      topic: function () {
+        xmlrpcBuilder.buildMethodCall('testMethod', [-32], this.callback)
+      }
+    , 'contains the negative integer' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><int>-32</int></value></param></params></methodCall>')
+      }
+    }
+  , 'with an Integer param of 0' : {
+      topic: function () {
+        xmlrpcBuilder.buildMethodCall('testMethod', [0], this.callback)
+      }
+    , 'contains 0' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><int>0</int></value></param></params></methodCall>')
+      }
+    }
     // Test Nil
   , 'with a Nil param' : {
       topic: function () {
@@ -103,13 +93,23 @@ vows.describe('XML-RPC Builder').addBatch({
         assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><nil/></value></param></params></methodCall>')
       }
     }
-    // Test Array
-  , 'with an Array param' : {
-      topic: function () {
-        xmlrpcBuilder.buildMethodCall('testMethod', [['string1', 3]], this.callback)
+    // Test String
+  , 'with a regular String param' : {
+      topic: function() {
+        xmlrpcBuilder.buildMethodCall('testMethod', ['testString'], this.callback)
       }
-    , 'contains the array' : function (err, xml) {
-        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><array><data><value><string>string1</string></value><value><int>3</int></value></data></array></value></param></params></methodCall>')
+    , 'contains the string' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><string>testString</string></value></param></params></methodCall>')
+      }
+    }
+    // FIXME Empty string causes warnings. Need to figure out what's the
+    // spec way to define an empty string
+  , 'with an empty String param' : {
+      topic: function () {
+        xmlrpcBuilder.buildMethodCall('testMethod', [''], this.callback)
+      }
+    , 'contains an empty string' : function (err, xml) {
+        assert.equal(xml, '<?xml version="1.0"?><methodCall><methodName>testMethod</methodName><params><param><value><string/></value></param></params></methodCall>')
       }
     }
     // Test Struct

6721ae36fb349e14c1d477d261c5c58c0110c81e
Brandon Alexander <baalexander@gmail.com>
2011-06-25 12:31:01 -0500

    Listens for errors on method call request. See Issue #12.
    Creates a test suite for Client. Initially tests an invalid local URI.

---
+++ test/client-test.js
@@ -0,0 +1,22 @@
+var vows   = require('vows')
+  , assert = require('assert')
+  , Client = require('../lib/client')
+
+vows.describe('Client').addBatch({
+  //////////////////////////////////////////////////////////////////////
+  // Test method call functionality
+  //////////////////////////////////////////////////////////////////////
+  'A method call' : {
+    // Test invalid internal URI to send method call to
+    'with an invalid internal URI' : {
+      topic: function () {
+        var client = new Client({ host: 'localhost', port: 9999, path: '/'}, false)
+        client.call('getArray', null, this.callback)
+      }
+    , 'contains the error' : function (error, value) {
+        assert.isObject(error)
+      }
+    }
+  }
+}).export(module)
+

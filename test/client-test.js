9960ff132fede12879fc713c4607bcb16485f3b1
Brandon Alexander <baalexander@gmail.com>
2011-07-25 22:43:20 -0500

    Accepts custom headers for the HTTP request.
    Slight change to ProjectMoon's fix to prevent overwriting passed in headers with
    the same attribute as the predefined headers. Adds a test case for the header
    passing. See Issue #15.

--- test/client-test.js
+++ test/client-test.js
@@ -4,9 +4,34 @@ var vows   = require('vows')
 
 vows.describe('Client').addBatch({
   //////////////////////////////////////////////////////////////////////
+  // Test Constructor functionality
+  //////////////////////////////////////////////////////////////////////
+  'A constructor' : {
+    // Test standard Client initialization
+    'with URI options only' : {
+      topic: function () {
+        var client = new Client({ host: 'localhost', port: 9999, path: '/'}, false)
+        return client.options
+      }
+    , 'contains the standard headers' : function (topic) {
+        assert.deepEqual(topic, { host: 'localhost', port: 9999, path: '/', method: 'POST', headers: { 'User-Agent': 'NodeJS XML-RPC Client', 'Content-Type': 'text/xml', 'Accept': 'text/xml', 'Accept-Charset' : 'UTF8'}})
+      }
+    }
+    // Test passing custom headers to the Client
+  , 'with options containing header params' : {
+      topic: function () {
+        var client = new Client({ host: 'localhost', port: 9999, path: '/', headers: { 'User-Agent': 'Testaroo', 'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==' }}, false)
+        return client.options
+      }
+    , 'does not overwrite the custom headers' : function (topic) {
+        assert.deepEqual(topic, { host: 'localhost', port: 9999, path: '/', method: 'POST', headers: { 'User-Agent': 'Testaroo', 'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==', 'Content-Type': 'text/xml', 'Accept': 'text/xml', 'Accept-Charset' : 'UTF8'}})
+      }
+    }
+  }
+  //////////////////////////////////////////////////////////////////////
   // Test method call functionality
   //////////////////////////////////////////////////////////////////////
-  'A method call' : {
+, 'A method call' : {
     // Test invalid internal URI to send method call to
     'with an invalid internal URI' : {
       topic: function () {

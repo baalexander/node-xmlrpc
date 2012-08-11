048ffea55f2195f8e18c7bc3d42e5a5b225980c3
GRaALe <graale@list.ru>
2012-08-11 04:31:00 +0400

    cookies support: add tests, remove obsolete code TODO: tests for client, readme.txt

--- test/cookies_test.js
+++ test/cookies_test.js
@@ -22,7 +22,7 @@ vows.describe('Cookies').addBatch({
         this.callback(null, cookies.get("c"));
       },
       'it shall return undefined': function(value) {
-        assert.isUndefined(value);
+        assert.isNull(value);
       }
     },
     'set cookie with expiration date in the future': {
@@ -46,10 +46,128 @@ vows.describe('Cookies').addBatch({
         this.callback(null, cookies.get("a"));
       },
       'it shall not be possible to get this value': function(value) {
-        assert.isUndefined(value)
+        assert.isNull(value)
+      }
+    }
+  },
+  'parseResponse': {
+    'parsing response without cookies': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.parseResponse({});
+        cookies.parseResponse({
+          'set-cookie': []
+        })
+        return "ok";
+      },
+      'shall not process any error': function(value) {
+        //ok
+      }
+    },
+    'parsing response with cookie without expiration date': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.parseResponse({
+          'set-cookie': [" name=value "]
+        });
+        return cookies.get("name");
+      },
+      'shall get value of the cookie': function(value) {
+        assert.equal(value, "value");
+      }
+    },
+    'response with cookie with expiration date': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.parseResponse({
+          'set-cookie': [" name=value ;Expires=Wed, 01 Jan 2070 00:00:01 GMT "]
+        });
+        return cookies;
+      },
+      'shall get value of the cookie': function(cookies) {
+        assert.equal(cookies.get("name"), "value");
+      },
+      'shall get expiration date of the cookie': function(cookies) {
+        assert.equal(cookies.getExpirationDate("name").toUTCString(), "Wed, 01 Jan 2070 00:00:01 GMT");
+      }
+    },
+    'response with cookie with expiration date and other fields': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.parseResponse({
+          'set-cookie': [" name=value ;some=thing;Expires=Wed, 01 Jan 2070 00:00:01 GMT ;any=thing "]
+        });
+        return cookies;
+      },
+      'shall get value of the cookie': function(cookies) {
+        assert.equal(cookies.get("name"), "value");
+      },
+      'shall get expiration date of the cookie': function(cookies) {
+        assert.equal(cookies.getExpirationDate("name").toUTCString(), "Wed, 01 Jan 2070 00:00:01 GMT");
+      }
+    },
+    'response with several cookies': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.parseResponse({
+          'set-cookie': [" name=value ", "name2=value2"]
+        });
+        return cookies;
+      },
+      'shall get value of all the cookies': function(cookies) {
+        assert.equal(cookies.get("name"), "value");
+        assert.equal(cookies.get("name2"), "value2");
+      }
+    }
+  },
+  'composeRequest': {
+    'when there are not cookies set': {
+      topic: new Cookies(),
+      'shall not set cookie header': function(topic) {
+        var headers = {};
+        topic.composeRequest(headers);
+        assert.isUndefined(headers['Cookie']);
+      }
+    },
+    'when there is one cookie': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.set("a", "b");
+        return cookies;
+      },
+      'shall set cookie header with name/value pair': function(topic) {
+        var headers = {};
+        topic.composeRequest(headers);
+        assert.equal(headers['Cookie'], 'a=b')
+      }
+    },
+    'when there are two cookies': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.set("a", "b");
+        cookies.set("c", "d");
+        return cookies;
+      },
+      'shall set cookies header with name/value pairs separated by semicolon': function(topic) {
+        var headers = {};
+        topic.composeRequest(headers);
+        assert.equal(headers['Cookie'], 'a=b;c=d')
+      }
+    },
+    'when some cookie is expired': {
+      topic: function() {
+        var cookies = new Cookies();
+        cookies.set("new", "one");
+        var date = new Date();
+        date.setFullYear(date.getFullYear()-1);
+        cookies.set("expired", "value", {expires: date});
+        return cookies;
+      },
+      'shall set cookies header with non-expired cookies only': function(topic) {
+        var headers = {};
+        topic.composeRequest(headers);
+        assert.equal(headers['Cookie'], 'new=one')
       }
     }
   }
-  //TODO: composeRequest/parseResponse
-  //TODO: server tests
 }).export(module);
